f18411a839178440c57e05829fe4bd8a8494badd
Devin Mullin <devin.mullin90@gmail.com>
2021-08-31 16:03:34 -0500

    commit tree 1b9317dbf346a42b3cf5cecbc046a0ec02f3029a

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+const hq = 42
+const feet = 264
+
+function distanceFromHqInBlocks(pickup){
+    if(pickup > hq){
+        return pickup - hq
+    } else if (pickup < hq){
+        return hq - pickup
+    }
+}
+
+function distanceFromHqInFeet(pickup){
+    return distanceFromHqInBlocks(pickup) * feet
+}
+
+function distanceTravelledInFeet(pickup, drop){
+    if(pickup > drop){
+        return (pickup - drop) * feet
+    } else {
+        return (drop - pickup) * feet
+    }
+}
+
+function calculatesFarePrice(pickup, drop){
+let calculateFt = distanceTravelledInFeet(pickup, drop)
+    if (calculateFt < 400){
+        return calculatesFarePrice = 0
+    } else if (calculateFt > 400 && calculateFt < 2000){
+        return (calculateFt - 400) * 0.02
+    } else if (calculateFt > 1999 && calculateFt < 2499){
+        return (25)
+    } else if (calculateFt > 2500){
+        return "cannot travel that far"
+    } 
+}
